2709fb19ee406e4c9e00553b03f54620c8215f42
suchithsaraaaa <suchith2005@gmail.com>
2025-05-14 03:05:45 +0530

    Update urls.py

--- video_chat_app/videochat/urls.py
+++ video_chat_app/videochat/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<str:room_name>/', views.room, name='room'),
+    path('', views.index, name='index'),               # root/homepage
+    path('<str:room_name>/', views.room, name='room'), # existing chat room route
 ]
