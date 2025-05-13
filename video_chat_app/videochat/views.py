a838d9d452ea5a97e2a71508a436ca9af9380891
suchithsaraaaa <suchith2005@gmail.com>
2025-05-14 03:07:57 +0530

    Update views.py

--- video_chat_app/videochat/views.py
+++ video_chat_app/videochat/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def room(request, room_name):
     return render(request, 'videochat/room.html', {'room_name': room_name})
+def index(request):
+    return render(request, 'videochat/index.html')
+
